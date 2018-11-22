3777bd04e9c86b980f95226a7a82c2f83149e083
Jadon Fowler <j@jadon.io>
2018-11-22 14:49:35 -0500

    Typing: Output of a function can be used as a function
    Instead of treating any variable as /a.a or /a./b.(a -> b),
    the amount of polytypes depends on how the variable is used.
    In ((x y) z), x : /a./b./c.(a -> (b -> c))
    
    \w.\y.\x.\z.(((w x) y) z) works!
    \x.\y.\z.(((x y) z) z) doesn't work yet
    
    Signed-off-by: Jadon Fowler <j@jadon.io>

--- src/main/kotlin/io/jadon/lambda/type.kt
+++ src/main/kotlin/io/jadon/lambda/type.kt
@@ -21,7 +21,8 @@ fun infer(exp: Expression, env: Environment, typeMap: MutableMap<String, Type> =
                 val type: Type? = env.variables.filterKeys { it.name == exp.argument }.entries.firstOrNull()?.key?.type
                 type ?: Untyped
             } else if (typeMap.containsKey(exp.argument)) {
-                typeMap.getOrElse(exp.argument) { Untyped }
+                val type = typeMap.getOrElse(exp.argument) { Untyped }
+                type
             } else {
                 Untyped
             }
@@ -38,59 +39,94 @@ fun infer(exp: Expression, env: Environment, typeMap: MutableMap<String, Type> =
                 return letter + if (charWraps > 0) charWraps else ""
             }
 
-            fun findUsage(needle: String, expression: Expression, isApplication: Boolean = false): Boolean {
+            fun findUsage(needle: String, expression: Expression, isApplication: Boolean = false): Int {
                 return when (expression) {
                     is Abstraction -> {
                         findUsage(needle, expression.value, false)
                     }
                     is Application -> {
-                        findUsage(needle, expression.function, true) || findUsage(needle, expression.argument, false)
+                        val function = expression.function
+                        var inFunction = findUsage(needle, function, true)
+                        val inArgument = findUsage(needle, expression.argument, false)
+
+                        if (function !is Term && inFunction > 0) {
+                            inFunction += 1
+                        }
+                        inFunction + inArgument
                     }
                     is Term -> {
                         if (expression.argument == needle) {
-                            isApplication
+                            if (isApplication) 1 else 0
                         } else {
-                            false
+                            0
                         }
                     }
                 }
             }
 
-            val isBeingUsedAsApplication = findUsage(exp.argument, exp.value)
+            val usagesAsFunction = findUsage(exp.argument, exp.value)
 
-            val f = if (isBeingUsedAsApplication) {
-                val a = t()
-                val b = t()
-                FunctionType(NamedType(a), NamedType(b))
+            var f = if (usagesAsFunction > 0) {
+                val names = (0..usagesAsFunction).map { t() }.reversed()
+                var parentType: Type = NamedType(names.first())
+                (1..usagesAsFunction).map { parentType = FunctionType(NamedType(names[it]), parentType) }
+                parentType
             } else {
-                val a = t()
-                NamedType(a)
+                NamedType(t())
             }
 
             typeMap[exp.argument] = f
 
             val bodyType = infer(exp.value, env, typeMap)
 
+            // update the type to see if we got any new information
+            // (specifically for inferences made in the application branch)
+            val maybeNewType = typeMap[exp.argument]
+            if (maybeNewType != null) {
+                f = maybeNewType
+            }
+
             val strippedBodyType = stripPolyTypes(bodyType)
             val bodyRealType = strippedBodyType.first
 
             // push polytypes to the front
-            val f2 = FunctionType(f, bodyRealType)
-            var wrapperPolyType: Type = f2
-            strippedBodyType.second.reversed().forEach { wrapperPolyType = PolyType(it, wrapperPolyType) }
+            var wrapperPolyType: Type = FunctionType(f, bodyRealType)
+            val polyTypeNamesInBody = strippedBodyType.second.reversed().distinct()
+            polyTypeNamesInBody.forEach { wrapperPolyType = PolyType(it, wrapperPolyType) }
 
-            if (isBeingUsedAsApplication && f is FunctionType) {
-                // /a.a -> bodyType
-                val a = (f.a as NamedType)
-                val b = (f.b as NamedType)
-                PolyType(a.name, PolyType(b.name, wrapperPolyType))
-            } else if (f is NamedType) {
+            if (usagesAsFunction > 0 && f is FunctionType) {
                 // /a./b.(a -> b) -> bodyType
+                var parentPolyType = wrapperPolyType
+                var child: Type = f
+                while (child is FunctionType) {
+                    val namedChildType = child.a as NamedType
+                    if (!polyTypeNamesInBody.contains(namedChildType.name)) {
+                        parentPolyType = PolyType(namedChildType.name, parentPolyType)
+                    }
+                    child = child.b
+                }
+                if (child is NamedType) {
+                    parentPolyType = PolyType(child.name, parentPolyType)
+                }
+                parentPolyType
+            } else if (f is NamedType) {
+                // /a.a -> bodyType
                 PolyType(f.name, wrapperPolyType)
             } else Untyped // shouldn't ever occur
         }
         is Application -> {
-            val functionType = infer(exp.function, env, typeMap)
+            var functionType = infer(exp.function, env, typeMap)
+
+            val argumentType = infer(exp.argument, env, typeMap)
+            val arg = exp.argument
+
+            if (functionType is FunctionType) {
+                if (functionType.a is NamedType && arg is Term) {
+                    typeMap[arg.argument] = functionType.a
+                } else if (functionType.a is FunctionType) {
+                    functionType = FunctionType(FunctionType((functionType.a as FunctionType).a, argumentType), functionType.b)
+                }
+            }
 
             fun returnType(type: Type): Type {
                 return when (type) {
